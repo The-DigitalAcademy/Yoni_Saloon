968870456d33a1a79347cd23633170638ab7ca2d
Rhendi <mudauml15@hotmail.com>
2024-09-02 14:48:51 +0200

    first commit

---
+++ src/app/UI/shared-ui/shared-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './shared-ui.component.html',
+  styleUrl: './shared-ui.component.css'
+})
+export class SharedUIComponent {
+
+}
