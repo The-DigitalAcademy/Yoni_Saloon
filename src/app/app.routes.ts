2734ef24f54cd9cb6952cbc773994e5dd97ef77a
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-09-09 08:53:44 +0200

    added a landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "", component: HomeComponent
+},
+{
+    path: "landing",
+    component: LandingComponent
+}];
