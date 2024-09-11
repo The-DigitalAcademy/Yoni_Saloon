b16bc7db13acd28bb9f1023626415280483adbba
JadeLuke <jadeluke.booysen@gmail.com>
2024-09-11 15:32:42 +0200

    changed button colours

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../../UI/navbar/navbar.component';
 import { FooterComponent } from '../../UI/footer/footer.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
 
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterLink, RouterLinkActive],
 
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
