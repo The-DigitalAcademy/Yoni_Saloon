a7c12c892ac6f4be7b7da9589dda03e7a72c430a
Nellark <nelakulati@gmail.com>
2024-09-11 16:29:55 +0200

    removed comments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router'; // Import Router for navigation
+import { Router, RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
