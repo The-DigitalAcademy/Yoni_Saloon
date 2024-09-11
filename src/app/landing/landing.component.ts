ef7596afe3754456af5d5a96da185ed5de7286a8
JadeLuke <jadeluke.booysen@gmail.com>
2024-09-11 15:08:20 +0200

    added footer

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarComponent } from "../UI/navbar/navbar.component";
+import { FooterComponent } from '../UI/footer/footer.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NavbarComponent],
+  imports: [RouterLink, RouterLinkActive, NavbarComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 
 
 import { NavbarComponent } from '../../UI/navbar/navbar.component';
+import { FooterComponent } from '../../UI/footer/footer.component';
 
 @Component({
   selector: 'app-services',
   standalone: true,
 
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
 
   templateUrl: './services.component.html',
   styleUrl: './services.component.css',
